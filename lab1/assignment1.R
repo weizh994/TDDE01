2f166cd4e2c1f90a19acd64d04678d5f1d77c7b5
mfroeh <mfroeh0@pm.me>
2022-11-15 08:13:03 +0100

    Updates

--- lab1/assignment1.R
+++ lab1/assignment1.R
@@ -1,9 +1,11 @@
-# setwd("/Users/Amir/Desktop/Liu/ML/L1")
+#setwd("~/dev/tdde01/lab1")
 
 # install.packages("kknn")
 library(kknn)
 
 ## Divide into train, valid and test sets
+
+## 1)
 data <- read.csv("optdigits.csv")
 n <- nrow(data)
 set.seed(12345)
@@ -16,6 +18,7 @@ valid <- data[id2, ]
 id3 <- setdiff(id1, id2)
 test <- data[id3, ]
 
+## 2)
 # Fit a kknn model to our training data and test it on the training data
 model_train <- kknn(as.factor(train[, 65]) ~ ., train = train, test = train, k = 30, kernel = "rectangular")
 
@@ -34,22 +37,15 @@ print(cm_test)
 miss_test <- nrow(test) - sum(diag(cm_test))
 miss_rate_test <- miss_test / nrow(test)
 
-# All the eights we predicted
-# temp <- which(as.numeric(as.character(model_train$fitted.values)) == 8)
- 
+## 3)
 # All the true eights
 temp <- which(as.numeric(train[,65]) == 8)
-
-# True and correctly predicted eights
-# temp <- which(as.numeric(train[,65]) == 8 && as.numeric(as.character(model_train$fitted.values)) == 8)
-
 eights_probs <- model_train$prob[temp, 9]
 eights_indices <- temp[order(eights_probs, decreasing = T)]
 
-# Three lowest confidence
-
 eights_count = length(eights_indices)    
 
+# Two lowest confidence
 lowest1 <- matrix(as.numeric(train[eights_indices[eights_count], -65]), 8, 8, byrow = T)
 lowest2 <- matrix(as.numeric(train[eights_indices[eights_count-1], -65]), 8, 8, byrow = T)
 lowest3 <- matrix(as.numeric(train[eights_indices[eights_count-2], -65]), 8, 8, byrow = T)
@@ -64,3 +60,32 @@ highest2 <- matrix(as.numeric(train[eights_indices[2], -65]), 8, 8, byrow = T)
 
 heatmap(highest1, Rowv = NA, Colv = NA)
 heatmap(highest2, Rowv = NA, Colv = NA)
+
+## 4)
+train_err <- c()
+valid_err <- c()
+for (k_ in 1:30) {
+  # Training 
+  model_train_k <- kknn(as.factor(train[, 65]) ~ ., train = train, test = train, k = k_, kernel = "rectangular")
+  cm_train_k <- table(train[, 65], model_train_k$fitted.values)
+  train_err <- c(train_err, (nrow(train) - sum(diag(cm_train_k))) / nrow(train))
+  
+  # Validation
+  model_valid_k <- kknn(as.factor(train[, 65]) ~ ., train = train, test = valid, k = k_, kernel = "rectangular")
+  cm_valid_k <- table(valid[, 65], model_valid_k$fitted.values)
+  valid_err <- c(valid_err, (nrow(valid) - sum(diag(cm_valid_k))) / nrow(valid))
+}
+
+print(train_err)
+print(valid_err)
+
+
+plot(x=1:30,y=train_err,col="red",type="o", lty=1)
+
+points(x=1:30, y=valid_err, col="blue", type="o")
+lines(x=1:30,y=valid_err,col="blue", lty=2)
+
+model_test_8 = kknn(as.factor(train[, 65])~., train = train, test = test, k = 8, kernel = "rectangular")
+cm_test_8 <- table(test[, 65], model_test_8$fitted.values)
+test_err <- (nrow(test) - sum(diag(cm_test_8))) / nrow(test)
+print(test_err)
