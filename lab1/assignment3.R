149d16867013f7cd594e3510703a804a5d0f3405
Weixiong <weizh994@student.liu.se>
2022-11-20 11:41:45 +0100

    fix some bugs in assignment3

--- lab1/assignment3.R
+++ lab1/assignment3.R
@@ -3,8 +3,12 @@ data=read.csv("pima-indians-diabetes.csv")
 colnames(data)[ncol(data)-1]="Age"
 colnames(data)[ncol(data)]="Diabetes"
 ## task1
-plot(data[data$Diabetes==0,ncol(data)-1],data[data$Diabetes==0,2],col="blue")
+plot(data[data$Diabetes==0,ncol(data)-1],data[data$Diabetes==0,2],col="blue",xlab = "Age",ylab="Plasma glucose concentration")
 points(data[data$Diabetes==1,ncol(data)-1],data[data$Diabetes==1,2],col="red")
+
+#plot(data[data$Diabetes==0,ncol(data)-1],data[data$Diabetes==0,2],col="blue",xlab = "Age",ylab="Plasma glucose concentration")
+#points(data[data$Diabetes==1,ncol(data)-1],data[data$Diabetes==1,2],col="red")
+
 
 ## task2
 colnames(data)[2]="Plasma"
@@ -16,11 +20,13 @@ Pred=ifelse(Prob>=r1, "Diabetes", "No Diabetes")
 Pred_table=table(data2$data.Diabetes, Pred)
 mc_error1=(Pred_table[1,1]+Pred_table[2,2])/sum(Pred_table)
 data2_2=data.frame(data$Plasma,data$Age,Prob)
-plot(data2_2[data2_2$Prob>=r1,2],data2_2[data2_2$Prob>=r1,1],col="red",xlim = range(min(data2_2$data.Age),max(data2_2$data.Age)),ylim = range(min(data2_2$data.Plasma),max(data2_2$data.Plasma)))
+#plot(data[data$Diabetes==0,ncol(data)-1],data[data$Diabetes==0,2],col="blue",xlab = "Age",ylab="Plasma glucose concentration")
+#points(data[data$Diabetes==1,ncol(data)-1],data[data$Diabetes==1,2],col="red")
+plot(data2_2[data2_2$Prob>=r1,2],data2_2[data2_2$Prob>=r1,1],col="red",xlim = range(min(data2_2$data.Age),max(data2_2$data.Age)),ylim = range(min(data2_2$data.Plasma),max(data2_2$data.Plasma)),xlab = "Age",ylab="Plasma glucose concentration")
 points(data2_2[data2_2$Prob<r1,2],data2_2[data2_2$Prob<r1,1],col="blue")
 data2_3=data.frame(1,data$Plasma,data$Age)
 z2=t(as.matrix(m2$coefficients))%*%t(data2_3)
-plot(z2,exp(z2)/(1+exp(z2)),xlim = range(-8,8))
+#plot(z2,exp(z2)/(1+exp(z2)),xlim = range(-8,8))
 
 slope <- -m2$coefficients[3]/m2$coefficients[2]
 intercept <- -m2$coefficients[1]/m2$coefficients[2]
@@ -28,21 +34,22 @@ abline(intercept,slope)
 ## task3
 ## a) g(x)=1-g(x)
 
-
-
+plot(data[data$Diabetes==0,ncol(data)-1],data[data$Diabetes==0,2],col="blue",xlab = "Age",ylab="Plasma glucose concentration")
+points(data[data$Diabetes==1,ncol(data)-1],data[data$Diabetes==1,2],col="red")
+abline(intercept,slope)
 
 ## task4
 r2=0.2
 Pred2=ifelse(Prob>=r2, "Diabetes", "No Diabetes")
 Pred_table2=table(data2$data.Diabetes, Pred2)
 mc_error2=(Pred_table2[1,1]+Pred_table2[2,2])/sum(Pred_table2)
-plot(data2_2[data2_2$Prob>=r2,2],data2_2[data2_2$Prob>=r2,1],col="red",xlim = range(min(data2_2$data.Age),max(data2_2$data.Age)),ylim = range(min(data2_2$data.Plasma),max(data2_2$data.Plasma)))
+plot(data2_2[data2_2$Prob>=r2,2],data2_2[data2_2$Prob>=r2,1],col="red",xlim = range(min(data2_2$data.Age),max(data2_2$data.Age)),ylim = range(min(data2_2$data.Plasma),max(data2_2$data.Plasma)),xlab = "Age",ylab="Plasma glucose concentration")
 points(data2_2[data2_2$Prob<r2,2],data2_2[data2_2$Prob<r2,1],col="blue")
 r3=0.8
 Pred3=ifelse(Prob>=r3, "Diabetes", "No Diabetes")
 Pred_table3=table(data2$data.Diabetes, Pred3)
 mc_error3=(Pred_table3[1,1]+Pred_table3[2,2])/sum(Pred_table3)
-plot(data2_2[data2_2$Prob>=r3,2],data2_2[data2_2$Prob>=r3,1],col="red",xlim = range(min(data2_2$data.Age),max(data2_2$data.Age)),ylim = range(min(data2_2$data.Plasma),max(data2_2$data.Plasma)))
+plot(data2_2[data2_2$Prob>=r3,2],data2_2[data2_2$Prob>=r3,1],col="red",xlim = range(min(data2_2$data.Age),max(data2_2$data.Age)),ylim = range(min(data2_2$data.Plasma),max(data2_2$data.Plasma)),xlab = "Age",ylab="Plasma glucose concentration")
 points(data2_2[data2_2$Prob<r3,2],data2_2[data2_2$Prob<r3,1],col="blue")
 
 
@@ -64,11 +71,12 @@ Pred5=ifelse(Prob5>=r1, "Diabetes", "No Diabetes")
 Pred_table5=table(data5$y, Pred5)
 mc_error5=(Pred_table5[1,1]+Pred_table5[2,2])/sum(Pred_table5)
 data5_2=data.frame(data$Plasma,data$Age,Prob5)
-plot(data5_2[data5_2$Prob>=r1,2],data5_2[data5_2$Prob>=r1,1],col="red",xlim = range(min(data5_2$data.Age),max(data5_2$data.Age)),ylim = range(min(data5_2$data.Plasma),max(data5_2$data.Plasma)))
+plot(data5_2[data5_2$Prob>=r1,2],data5_2[data5_2$Prob>=r1,1],col="red",xlim = range(min(data5_2$data.Age),max(data5_2$data.Age)),ylim = range(min(data5_2$data.Plasma),max(data5_2$data.Plasma)),xlab = "Age",ylab="Plasma glucose concentration")
 points(data5_2[data5_2$Prob<r1,2],data5_2[data5_2$Prob<r1,1],col="blue")
-data5_3=data.frame(1,x1,x2,z1,z2,z3,z4,z5)
-z5=t(as.matrix(m5$coefficients))%*%t(as.matrix(data5_3))
+#data5_3=data.frame(1,x1,x2,z1,z2,z3,z4,z5)
+#z5=t(as.matrix(m5$coefficients))%*%t(as.matrix(data5_3))
 #plot(z5,exp(z5)/(1+exp(z5)),xlim = range(-8,8))
-slope <- -m5$coefficients[3]/m5$coefficients[2]
-intercept <- -m5$coefficients[1]/m5$coefficients[2]
-abline(intercept,slope)
+#slope <- -m5$coefficients[3]/m5$coefficients[2]
+#intercept <- -m5$coefficients[1]/m5$coefficients[2]
+#abline(intercept,slope)
+
